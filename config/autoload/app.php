d4a0cf4db30ec4daf1abda19b13c8f064aaa6226
ZhuChongLin <8210856@qq.com>
2024-03-23 16:44:51 +0800

    添加配置

--- config/autoload/app.php
+++ config/autoload/app.php
@@ -10,4 +10,10 @@ return [
   'default_timezone' => env('default_timezone', 'Asia/Shanghai'),
   // 是否开启调试模式
   'debug' => env('app_debug', true),
+  // 服务提供者注册
+  'providers' => [
+
+  ],
+  // 命令处理程序注册
+  'commands' => []
 ];
