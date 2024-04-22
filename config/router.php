ebd19099a8a314a0072f7a1f819d20d15c17670a
ZhuChongLin <8210856@qq.com>
2024-04-22 15:35:34 +0800

    完善路由

--- config/router.php
+++ config/router.php
@@ -22,10 +22,16 @@ return [
   'case_sensitive' => false,
   // 伪静态后缀，支持通过数组设置多个。
   'suffix' => '*',
-  // 域名校验 例如 www.baidu.com
-  'domain' => [],
+  // 域名校验 例如 [www.baidu.com]
+  'domain' => '*',
   // 默认的路由变量正则表达式
-  'default_variable_regex' => '[\w\.]+',
-  // 路由配置文件列表
-  'route_config_files' => [],
+  'default_pattern_regex' => '[\w\.]+',
+  // 要加载的路线配置文件
+  'route_config_files' => [
+    BASE_PATH . '/app/route.php'
+  ],
+  // 是否缓存路由线路
+  'cache_route_config' => true,
+  // 路由线路缓存存放目录
+  'cache_route_dir' => BASE_PATH . '/runtime/route',
 ];
