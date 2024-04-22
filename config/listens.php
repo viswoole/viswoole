7578c92a2d132b03d777d8e6c9755de2092c0b39
ZhuChongLin <8210856@qq.com>
2024-04-22 17:06:32 +0800

    优化

--- config/listens.php
+++ config/listens.php
@@ -1,7 +1,7 @@
 <?php
 // +----------------------------------------------------------------------
 // | 全局事件监听注册
-// | return['event'=>[class,method],'event'=>listen::class,'event'=>callback]
+// | return['event'=>listen::class|'event'=>callback]
 // +----------------------------------------------------------------------
 
 declare (strict_types=1);
@@ -10,5 +10,9 @@ return [
   // 应用初始化
   'AppInit' => [],
   // 应用销毁
-  'AppDestroyed' => []
+  'AppDestroyed' => [],
+  // 服务器启动
+  'ServerStart' => [],
+  // 服务器停止
+  'ServerShutdown' => []
 ];
