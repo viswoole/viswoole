feb6da75c26b4f4100ded33107a2abb06400de6b
ZhuChongLin <8210856@qq.com>
2024-04-26 14:00:20 +0800

    任务管理

---
+++ app/task.php
@@ -0,0 +1,8 @@
+<?php
+// +----------------------------------------------------------------------
+// | 任务注册
+// | 示例1：\ViSwoole\Core\Facade\Task::register('sendLoginCode', [sendCode::class,'login'])
+// | 示例2：\ViSwoole\Core\Facade\Task::registers('sendCode', sendCode::class)
+// +----------------------------------------------------------------------
+
+declare (strict_types=1);
