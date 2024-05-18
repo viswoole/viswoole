6feacd24ea42fbea19d7c53a017e80b78dc752cb
ZhuChongLin <8210856@qq.com>
2024-05-18 14:44:37 +0800

    update

--- app/Response.php
+++ app/Response.php
@@ -15,6 +15,10 @@ declare (strict_types=1);
 
 namespace App;
 
+use JsonSerializable;
+use Override;
+use ViSwoole\HttpServer\Contract\ResponseInterface;
+
 /**
  * 自定义响应类
  */
@@ -24,4 +28,23 @@ class Response extends \ViSwoole\HttpServer\Response
    * @var bool 是否将响应输出到控制台
    */
   protected bool $echoToConsole = false;
+
+  /**
+   * 该方法用于异常时输出响应内容
+   *
+   * @param string $errMsg 错误信息
+   * @param int $errCode 业务错误码
+   * @param int $statusCode 状态码
+   * @param array|JsonSerializable|null $errTrace 错误追踪信息
+   * @return ResponseInterface
+   */
+  #[Override] public function exception(
+    string                 $errMsg = '系统内部异常',
+    int                    $errCode = 500,
+    int                    $statusCode = 500,
+    array|JsonSerializable $errTrace = null
+  ): ResponseInterface
+  {
+    return $this->json(compact('errMsg', 'errCode', 'errTrace'), $statusCode);
+  }
 }
