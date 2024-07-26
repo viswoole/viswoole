c75ec3fec548d67c1629df9ad4cb7f206df010fd
ZhuChongLin <8210856@qq.com>
2024-07-26 12:26:39 +0800

    示例

---
+++ app/Controller/Example.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace App\Controller;
+
+use App\Response;
+use Viswoole\HttpServer\Contract\ResponseInterface;
+use Viswoole\Router\Annotation\AutoRouteController;
+
+/**
+ * 示例
+ */
+#[AutoRouteController] class Example
+{
+  public static function test(\App\Interface\Example $info, Response $response): ResponseInterface
+  {
+    return $response->html('<h1>Hello ' . $info->name . '</h1>');
+  }
+}
+
---
+++ app/Interface/Example.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | Viswoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024 https://viswoole.com All rights reserved.
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace App\Interface;
+
+use Viswoole\Core\Validate\Rules\Chinese;
+
+/**
+ * 该类用于演示将类，用于参数校验
+ */
+class Example
+{
+  /**
+   * @param string $name 名称
+   */
+  public function __construct(
+    #[Chinese] public string $name
+  )
+  {
+  }
+}
