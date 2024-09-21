d699b6f7b172151839135522eeac39a159552ca9
ZhuChongLin <8210856@qq.com>
2024-09-21 23:11:13 +0800

    简化数据库配置并更新注释
    - 移除数据库配置文件头部的作者信息。
    - 'channel'配置，更名为'channels'，并支持使用数组格式配置通道。
    - 更新相关注释，以匹配新的配置结构。
    - 修正PDOChannel构造参数中的密码默认值为123456。

--- config/database.php
+++ config/database.php
@@ -16,14 +16,18 @@ return [
   // 调试信息保存方式，1保存到控制台，2保存到日志文件，3保存到控制台和日志文件
   'info_save_manner' => Db::DEBUG_SAVE_CONSOLE | Db::DEBUG_SAVE_LOGGER,
   // 通道列表
-  'channel' => [
-    // 驱动类需继承Viswoole\Database\Channel
-    'default' => new PDOChannel(
-      host    : env('DATABASE_HOST', '127.0.0.1'),
-      port    : (int)env('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', ''),
-      username: env('DATABASE_USER', 'root'),
-      password: env('DATABASE_PASSWORD', '123456'),
-    )
+  'channels' => [
+    'default' => [
+      // 通道类，必须继承Viswoole\Database\Channel
+      'channel' => PDOChannel::class,
+      // PDOChannel通道构造参数
+      'options' => [
+        'host' => env('DATABASE_HOST', '127.0.0.1'),
+        'port' => (int)env('DATABASE_PORT', 3306),
+        'database' => env('DATABASE_NAME', ''),
+        'username' => env('DATABASE_USER', 'root'),
+        'password' => env('DATABASE_PASSWORD', '123456'),
+      ]
+    ]
   ]
 ];
